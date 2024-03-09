47e6293ffe2c5cb09f63bfb39e2a2e5333c2fcd6
tushar <tusharkumarshah14394@gmail.com>
2024-03-09 16:26:29 +0530

    placeholder

--- src/sections/RegisterForm_new.jsx
+++ src/sections/RegisterForm_new.jsx
@@ -16,18 +16,26 @@ export default function RegisterForm() {
   return (
     <section
       id="Register"
-      className="h-[100vh] w-[100%] box-border p-5 pt-5 md:pb-5 mb-[-15vh] lg:mb-0 relative"
+      className="min-h-screen w-full box-border pt-5 md:pb-5 mb-[-15vh] lg:mb-0 relative"
     >
-      <span className="capitalize text-[#FF692D]">&#47;&#47; game on</span>
+      {/* <span className="capitalize text-[#FF692D]">&#47;&#47; game on</span>
       <div className="flex h-full w-full gap-3 lg:gap-0">
         <div className="w-full hidden lg:block h-full">
-          <h2 className="uppercase text-5xl font-bold text-white">register</h2>
+          <h2 className="uppercase text-5xl font-bold text-white">&gt;&gt; register</h2>
         </div>
+      </div> */}
+      <div id="prize" className="text-white lg:px-36 p-8">
+      <h1 className="lg:text-[20px] md:text-[14px] font-BebasNeue ">
+        &#47;&#47; game on
+        </h1>
+        <h1 className="text-[3vmax] text-[#FF431A] font-BebasNeue ">
+          &gt;&gt; Register
+        </h1>
       </div>
 
-      <div className="absolute flex mt-10 flex-col lg:p-10 lg:pl-[10vw]  md:flex-row gap-10 lg:flex-row items-center justify-between lg:w-[full] lg:h-[auto] w-[80%] top-[10vh] z-2 h-full  ">
-        <div className="hidden md:flex lg:flex justify-center items-center">
-          <Lottie options={defaultOptions} height={300} width={300} />
+      <div className="absolute flex mt-10 flex-col lg:p-10 lg:pl-[18vw]  md:flex-row gap-4 lg:flex-row items-center justify-between lg:w-[full] lg:h-[auto] w-[80%] top-[10vh] z-2 h-full  ">
+        <div className="hidden md:flex lg:flex justify-right items-right">
+          <Lottie options={defaultOptions} height={400} width={400} />
         </div>
         <FormProvider>
           <Form />
